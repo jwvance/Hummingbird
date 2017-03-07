dc29a48c8d64ae70a05c18fe0637a6c945218931
Jason Vance <jwvance@ucsc.edu>
2017-03-07 09:13:39 -0800

    Got DMA 1.0 working

--- PSOC_Jason/Hummingbird Test.cydsn/main.c
+++ PSOC_Jason/Hummingbird Test.cydsn/main.c
@@ -13,30 +13,30 @@
 #include <stdio.h>
 #include <string.h>     
 #include <stdlib.h>
-#include <WAV.h>
 #include <FS.h>
+#include <math.h>
 
 int16 ADCoutput;
 
 #define DATA_LENGTH 1024u
-short dataFrame1[DATA_LENGTH] = {};
-short dataFrame2[DATA_LENGTH] = {};
+int16 dataFrame1[DATA_LENGTH] = {};
+double dataFrame2[DATA_LENGTH] = {};
 uint16 frameIndex = 0;
 uint8 currFrame = 0;
 
+// DMA Configuration for DMA_1 
+#define DMA_1_BYTES_PER_BURST 2
+#define DMA_1_REQUEST_PER_BURST 1
+#define DMA_1_SRC_BASE (CYDEV_PERIPH_BASE)
+#define DMA_1_DST_BASE (CYDEV_SRAM_BASE)
+
 char8 LCDstr[10];
 int16 bufferFull = 0;
-uint8 frameCompleteFlag=0;
-
-FS_FILE * pFile;
-FILE *wav_sample;
-const char *file = "wav_sample";
-
 
 CY_ISR_PROTO(GetSample);  //declare the interrupt
 
-const char *byte_to_binary(int x)
-{
+//convert a decimal to binary for printing binary strings
+const char *byte_to_binary(int x){
     static char b[9];
     b[0] = '\0';
 
@@ -48,7 +48,6 @@ const char *byte_to_binary(int x)
 
     return b;
 }
-
 
 int SizeOfString(char data[]){
     int i = 0;
@@ -66,44 +65,63 @@ int SizeOfString(char data[]){
     return size;
 }
 
+void DMA_Config(){    
+    /* Variable declarations for DMA_1 */
+    uint8 DMA_1_Chan;
+    uint8 DMA_1_TD[1] = {0};
+
+    /* Iniitialize DMA channel */
+    DMA_1_Chan = DMA_1_DmaInitialize(DMA_1_BYTES_PER_BURST, DMA_1_REQUEST_PER_BURST,
+                                     HI16(DMA_1_SRC_BASE), HI16(DMA_1_DST_BASE));
+
+    /* Allocate TD */
+    DMA_1_TD[0] = CyDmaTdAllocate();
+
+    /* TD configuration setting */
+    CyDmaTdSetConfiguration(DMA_1_TD[0], 2u, DMA_1_TD[0], DMA_1__TD_TERMOUT_EN | TD_INC_DST_ADR);
+
+    /* Set Source and Destination address */
+    CyDmaTdSetAddress(DMA_1_TD[0], LO16((uint32)ADC_SAR_1_SAR_WRK0_PTR),
+                      LO16((uint32)dataFrame1));  //VDAC8_1_Data_PTR
+
+    /* TD initialization */
+    CyDmaChSetInitialTd(DMA_1_Chan, DMA_1_TD[0]);
+
+    /* Enable the DMA channel */
+    CyDmaChEnable(DMA_1_Chan, 1u);
+}
+
 int main()
 {    
-    // Enable global interrupts 
-    CyGlobalIntEnable;
-    
-    // Start the file system
-    FS_Init();
-    
-    // Start the LCD
-    LCD_Start();
-    
-    // Start the data sample timer
-    Timer_1_Start();
-    
-    // Start the ADC conversion
-    ADC_SAR_1_Start();
+    CyGlobalIntEnable; // Enable global interrupts 
+    isr_1_StartEx(GetSample);  // Start the ADC sampling interrupt
+    FS_Init(); // Start the file system 
+    LCD_Start(); // Start the LCD
+    Timer_1_Start(); // Start the data sample timer
+    ADC_SAR_1_Start();  // Start the ADC conversion
     ADC_SAR_1_StartConvert();
+    DMA_Config();  //config the DMA
     
-    // Start the interrupt
-    isr_1_StartEx(GetSample);
-    
-    // Display the value of ADC output on LCD 
-    //LCD_Position(0u, 0u);
-    //LCD_PrintString("ADC_Output:");
-    
-    //create .wav file, write all bytes to file
-    //wav_sample = wavfile_open(file);
-    //wavfile_write(wav_sample, dataFrame, DATA_LENGTH);
-    //wavfile_close(wav_sample);  
-    
-    pFile = FS_FOpen("wamples.txt", "w");
+    FS_FILE * pFile;
+    pFile = FS_FOpen("samples.txt", "w");
     
     for(;;)
     {
+        /*
+        LCD_Position(0u, 0u);
+        LCD_PrintString("ADC Output: ");
+        LCD_Position(1u, 0u);
+        LCD_PrintString("DAC Input: ");
+        // Print the ADC output value on LCD 
+        LCD_Position(0u, strlen("ADC Output: "));
+        LCD_PrintInt16(ADC_SAR_1_GetResult16());
+
+        // Print the DAC Data register value 
+        LCD_Position(1u, strlen("DAC Input: "));
+        LCD_PrintInt16(dataFrame1[4]); //VDAC8_1_Data
+        */
         
-        
-        // Skips the file writing until the buffer is full
-        if(bufferFull){
+        if(dataFrame1[DATA_LENGTH-1] != 0){
             char data [16];
             uint i;
             // Fills the file with the data from the buffer
@@ -111,19 +129,56 @@ int main()
                 sprintf(data, "%d\n", dataFrame1[i]);
                 FS_Write(pFile, data, SizeOfString(data));
             }
-            bufferFull = 0;    // Stops it from continuing to print to file
+            //bufferFull = 0;    // Stops it from continuing to print to file
         
-             FS_FClose(pFile);
+            FS_FClose(pFile);
         
             sprintf(LCDstr, "Write Complete");
             LCD_Position(0u, 0u);
             LCD_PrintString(LCDstr);
-        
         }
-        
-       
-        
-       
+        // Skips the file writing until the buffer is full
+        //if(bufferFull)
+        {                        
+            //initialize fft arrays
+            /*double fftInput[SAMPLE_LEN];
+            int loop;
+            for(loop=0; loop<SAMPLE_LEN; loop++){
+                fftInput[loop]=dataFrame1[loop];   
+            }
+            
+            //Populates Note-MIDI array (index=midi note, value=frequency
+            double noteTable[MIDI_LEN];
+            //populate_midi_array(noteTable);
+            
+            //Initilize the Frequency Variable that will be computed by pitch.c functions
+            double pitchHz;
+            
+            // RUNS FFT HERE, using pitch_fft function from pitch.h
+            //pitchHz = pitch_fft(fftInput, sampImg);
+            // Finds MIDI note using midi.h function and creates MIDI array
+            //double midiNote = midi_note_from_freq(noteTable,pitchHz);
+    	 
+            LCD_ClearDisplay();
+            LCD_Position(0,0);
+            LCD_PrintNumber(pitchHz);
+            
+            CyDelay(1000);
+            
+            // Reset the fftInput the sample
+            int i;
+            for(i=0; i<SAMPLE_LEN; i++)
+            {
+                //fftInput[i]=sampReal[i];  
+                sampImg[i] = 0;
+            }
+            //isr_1_StartEx(GetSample);
+            bufferFull = 0;
+            */
+            
+            
+            
+        }        
     }
 }
 
@@ -131,11 +186,11 @@ CY_ISR(GetSample){
     /* Clear pending Interrupt */
     isr_1_ClearPending();
     
-    if(ADC_SAR_1_IsEndConversion(ADC_SAR_1_WAIT_FOR_RESULT)) {
+    if(ADC_SAR_1_IsEndConversion(ADC_SAR_1_WAIT_FOR_RESULT) && !bufferFull) {
         ADCoutput = ADC_SAR_1_GetResult16();
         // Pushes the sample onto the frame (AKA buffer) if we have room
         if(frameIndex < DATA_LENGTH){
-            dataFrame1[frameIndex] = ADCoutput;    
+            dataFrame1[frameIndex] = (double)ADCoutput;    
             frameIndex++;
             
         }else{          // Tells the main our buffer is full, resets frame index
@@ -146,8 +201,7 @@ CY_ISR(GetSample){
 //            }
             bufferFull = 1;
             frameIndex = 0;
-
-            
+            //isr_1_Stop();
         }
     }  
 }
